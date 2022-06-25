44d21ad0c9476de965c9e0465a7cce923646dcd5
SreevaniKanala19 <107243573+SreevaniKanala19@users.noreply.github.com>
2022-06-25 12:44:09 +0530

    add this file (#8)

--- practice_all.py
+++ practice_all.py
@@ -534,7 +534,15 @@ print(a)
 print(a[2])
 
 
-
+ip = 'aabbccddeeffgg'
+op = ''
+prev = ''
+for i in ip:
+    if i != prev and i not in op:
+        op = op+i+str(ip.count(i))
+    else:
+        prev = i
+print(op)
 
 
 
